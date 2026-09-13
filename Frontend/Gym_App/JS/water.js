dc3793b3fa1fcd2dab042ba0f34c65943129b496
agostinasolla <2502416@ucc.edu.ar>
2026-09-13 10:56:19 -0300

    delete wrong folders

--- Frontend/Gym_App/JS/water.js
+++
@@ -1,6 +0,0 @@
-function calcular(){
-    var peso = parseFloat(document.getElementById("peso").value);
-    var result =peso*0.033
-    var agua= result.toFixed(2);
-    document.getElementById("agua").value = agua;
-}
